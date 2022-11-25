4c195b0b7c6c11bf2e2217eaee9a7d769569d84c
Parmjot <Parmjot.seraphic@gmail.com>
2022-11-25 16:08:45 +0530

    Still work on Darkview

--- src/Components/AdminPages/UserVerification.js
+++ src/Components/AdminPages/UserVerification.js
@@ -53,49 +53,18 @@ const UserVerification = () => {
                             </div>
                         </div>
                     </div>
-                    <div className="verification-media-upload">
-                        <div className="mediabar">
-                            <img src="/images/user-verify.png" alt="img"/>
-                        </div>
-                        <div className="verify-content">
-                            <div className="formbar">
-                                <label>E-mail:</label>
-                                <h5>amans@gmail.com</h5>
+                </div>
+                <div className="verification-bar mt-5">
+                    <div className="user-detail-bar">
+                        <div className="detailleft">
+                            <div className="avatar-img">
+                                <img src="/images/user.png" alt="user-img" />
                             </div>
-                            <div className="formbar">
-                                <label>Phone number:</label>
-                                <h5>+91 9874563210</h5>
-                            </div>
-                            <div className="formbar">
-                                <label>Number Assigned for verification:</label>
-                                <h5>45</h5>
-                            </div>
-                            <div className="formbarbtn">
-                                <Button className="bg-primary text-white">Verify</Button>
-                                <Button className="outline-primary text-white">Decline</Button>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="verification-media-upload">
-                        <div className="mediabar">
-                            <img src="/images/user-verify.png" alt="img"/>
-                        </div>
-                        <div className="verify-content">
-                            <div className="formbar">
-                                <label>E-mail:</label>
-                                <h5>amans@gmail.com</h5>
-                            </div>
-                            <div className="formbar">
-                                <label>Phone number:</label>
-                                <h5>+91 9874563210</h5>
-                            </div>
-                            <div className="formbar">
-                                <label>Number Assigned for verification:</label>
-                                <h5>45</h5>
-                            </div>
-                            <div className="formbarbtn">
-                                <Button className="bg-primary text-white">Verify</Button>
-                                <Button className="outline-primary text-white">Decline</Button>
+                            <div className="user-detail">
+                                <h4 className="text-secondry">Amanpreet Singh</h4>
+                                <div className="user-availbility">
+                                    <h6 className="text-lightgray">@amans</h6>
+                                </div>
                             </div>
                         </div>
                     </div>
