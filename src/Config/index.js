00c4b94ad89946f35a3a19e6a1cce7d24f66f7d7
Jatinder Kaur <youshaarif13@gmail.com>
2023-03-15 15:53:23 +0530

    ui

--- src/Config/index.js
+++ src/Config/index.js
@@ -1,9 +1,9 @@
 //Backend URL:
 
 //local
-export const BASE_URL = "http://localhost:4000";
+//export const BASE_URL = "http://localhost:4000";
 //Stagging
-// export const BASE_URL = "https://dygres.dev.seraphic.io/node";
+export const BASE_URL = "https://dygres.dev.seraphic.io/node";
 //Live
 // export const BASE_URL = "http://54.164.159.66:4000";
 // export const BASE_URL = "https://api.dygres.com";
