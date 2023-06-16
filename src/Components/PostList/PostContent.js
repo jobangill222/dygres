d676b8f0987355b4cc4656dd17fc449ae0551104
joban <joban.seraphic@gmail.com>
2023-06-16 11:37:00 +0530

    post format fav icon and title

--- src/Components/PostList/PostContent.js
+++ src/Components/PostList/PostContent.js
@@ -6,11 +6,24 @@ const PostContent = ({ postContent }) => {
   // console.log('postContentpostContent', postContent)
   // const finalPostContent = highlightName(postContent);
 
+  const lines = postContent.split('\n');
+
   return (
     <>
       <div className="Description-bar">
         <p>
-          <HighLight content={postContent} />
+          {/* <HighLight content={postContent} /> */}
+
+          <div>
+            {lines.map((line, index) => (
+              <React.Fragment key={index}>
+                {/* {line} */}
+                <HighLight key={index} content={line} />
+                <br />
+              </React.Fragment>
+            ))}
+          </div>
+
         </p>
       </div>
 
