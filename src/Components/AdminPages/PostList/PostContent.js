74abf1377573b1efe2b27cc4598c7527a9db2871
joban <joban.seraphic@gmail.com>
2023-06-16 11:48:41 +0530

    admin panel post format

--- src/Components/AdminPages/PostList/PostContent.js
+++ src/Components/AdminPages/PostList/PostContent.js
@@ -2,11 +2,22 @@ import React from "react";
 import HighLight from "../HighLight";
 const PostContent = ({ postContent }) => {
 
+
+  const lines = postContent.split('\n');
+
   return (
     <>
       <div className="Description-bar">
         <p>
-          <HighLight content={postContent} />
+          {/* <HighLight content={postContent} /> */}
+
+          {lines.map((line, index) => (
+            <React.Fragment key={index}>
+              {/* {line} */}
+              <HighLight key={index} content={line} />
+              <br />
+            </React.Fragment>
+          ))}
         </p>
       </div>
 
